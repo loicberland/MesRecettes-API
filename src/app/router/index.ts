1ecde740cb61e3bfef0a461936d25079d7fa55f9
loicberland <berland.loic@yahoo.fr>
2023-03-02 13:37:41 +0100

    [test] test connection avec client PG en affichant sur la route / une table de test

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,8 +1,17 @@
 import express from 'express'
+import client from '../clients/pg'
 const router = express.Router()
 
-router.get('/', (req, res) => {
-	res.send('hello')
+// >>>>>>>>>>Test client PG<<<<<<<<<<<<<<
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.get('/', async (req, res): Promise<void> => {
+	const result = await client.query(`
+select * from "test"
+`)
+	res.json(result.rows)
 })
+// router.get('/', (req, res) => {
+// res.send('hello')
+// })
 
 export default router
